07afcef9fcd355243c640d17cc9a8b3d4154ac5a
wellwind <wellwind@mail2000.com.tw>
2018-05-04 15:14:39 +0800

    使用 [ngClass] 變更 CSS 樣式

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   keyword = '';
+  highlightTitle = false;
 
   constructor() { }
 
@@ -16,5 +17,7 @@ export class HeaderComponent implements OnInit {
 
   search($event) {
     console.log($event);
+
+    this.highlightTitle = !this.highlightTitle;
   }
 }
