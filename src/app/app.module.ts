21ecddc829a9b9ed28ac61f4f734be3e770b56d6
wellwind <wellwind@mail2000.com.tw>
2018-05-13 13:02:19 +0800

    加入 HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,17 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { ArticlesComponent } from './articles/articles.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { TagsComponent } from './tags/tags.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    ArticlesComponent,
-    TagsComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, ArticlesComponent, TagsComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
